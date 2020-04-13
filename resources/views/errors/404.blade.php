8528d28850a72dcfd287c8620f9d3381f0c29fa7
blind3y3 <24352318+blind3y3@users.noreply.github.com>
2020-04-14 00:38:14 +0500

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout')
+@section('content')
+
+    <div class="d-flex justify-content-center align-items-center" style="height: 70vh;">
+        <h1 class="mr-3 pr-3 align-top border-right inline-block align-content-center">404</h1>
+        <div class="inline-block align-middle">
+            <h2 class="font-weight-normal lead">The page you requested was not found.</h2>
+        </div>
+    </div>
+
+@endsection
